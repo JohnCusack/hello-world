330b19df75ac123798ecf02c01130e54ce0b2e8f
zhaoyibin <zhaoyibin@le.com>
2016-05-30 00:54:18 +0800

    tx config.

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ hello world in Java.
 
 ## mybatis-spring
 ** http://www.mybatis.org/spring/zh/getting-started.html **
+
+### mysql ref
+** https://dev.mysql.com/doc/refman/5.6/en/postinstallation.html **
--- mybatis-spring/src/main/java/com/hello/mapper/UserMapper.java
+++ mybatis-spring/src/main/java/com/hello/mapper/UserMapper.java
@@ -8,6 +8,6 @@ import org.apache.ibatis.annotations.Select;
  * Created by peker on 16/5/27.
  */
 public interface UserMapper {
-    @Select("SELECT * FROM users WHERE id = #{userId}")
-    User getUser(@Param("userId") String userId);
+    @Select("SELECT * FROM user WHERE id = #{userId}")
+    User getUser(@Param("id") String userId);
 }
